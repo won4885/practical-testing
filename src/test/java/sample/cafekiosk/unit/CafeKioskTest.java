892ba61886bcc2a1a5927d9d5e9695664b10a400
Sully <2dcoder@naver.com>
2023-10-08 22:32:01 +0900

    comment: BDD 스타일 주석 추가
    - 이렇게 하면 DisplayName도 명확하게 작성할 수 있음

--- src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
+++ src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.time.LocalDateTime;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import sample.cafekiosk.unit.beverage.Americano;
@@ -21,6 +22,7 @@ class CafeKioskTest {
 		System.out.println(">>> 담긴 음료: " + cafeKiosk.getBeverages().get(0).getName());
 	}
 
+	@DisplayName("음료 1개 추가 테스트")
 	@Test
 	void add() {
 		CafeKiosk cafeKiosk = new CafeKiosk();
@@ -77,8 +79,10 @@ class CafeKioskTest {
 		assertThat(cafeKiosk.getBeverages()).isEmpty();
 	}
 
+	@DisplayName("주문 목록에 담긴 상품들의 총 금액을 계산할 수 있다.")
 	@Test
 	void calculateTotalPrice() {
+		// given
 		CafeKiosk cafeKiosk = new CafeKiosk();
 		Americano americano = new Americano();
 		Latte latte = new Latte();
@@ -86,8 +90,10 @@ class CafeKioskTest {
 		cafeKiosk.add(americano);
 		cafeKiosk.add(latte);
 
+		// when
 		int totalPrice = cafeKiosk.calculateTotalPrice();
 
+		// then
 		assertThat(totalPrice).isEqualTo(8500);
 	}
 
